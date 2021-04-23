9dfae40bcd5bce695e32c51724b542bae67c2e11
Farid Ahmadpour <aramain.koramain@gmail.com>
2021-04-23 20:10:32 +0430

    Singleton arch over model loading

--- crawler/AmSentimentAnalyzer.py
+++ crawler/AmSentimentAnalyzer.py
@@ -6,15 +6,26 @@ import tensorflow_hub as hub
 import tensorflow_text as text
 from official.nlp import optimization  # to create AdamW optmizer
 
+load_model = True
+
 
 class AmSentiment:
-    saved_model_path = "/home/tesla/Project/SpideriFy/spiderify-backend/commen/ML-Model"
-    reloaded_model = tf.saved_model.load(saved_model_path)
-
     def __init__(self, saved_model_path):
         if saved_model_path is not None:
             self.saved_model_path = saved_model_path
+        initialize_sentiment()
 
     def sent_analyz(self, reviews: list):
         reloaded_results = tf.sigmoid(reloaded_model(tf.constant(reviews)))
         return reloaded_results.numpy().tolist()
+
+
+def initialize_sentiment():
+
+    global load_model, reloaded_model, saved_model_path
+    if load_model:
+        load_model = False
+        saved_model_path = "/home/tesla/Project/SpideriFy/spiderify-backend/commen/ML-Model"
+        reloaded_model = tf.saved_model.load(saved_model_path)
+
+    return
