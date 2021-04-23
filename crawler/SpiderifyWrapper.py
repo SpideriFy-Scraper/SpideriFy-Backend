98724adb1e463e6479760f71b96312fea542dc96
Farid Ahmadpour <aramain.koramain@gmail.com>
2021-04-23 17:59:39 +0430

    Integration with Amazon Spider class

--- crawler/SpiderifyWrapper.py
+++ crawler/SpiderifyWrapper.py
@@ -4,10 +4,11 @@ from crawler.AmazonSpider import AmazonSpider
 class SpiderifyWrapper:
 
     AMAZONSPIDER = None
-    DATA = {}
+    DATA = None
 
     def __init__(self, url: str):
         self.AMAZONSPIDER = AmazonSpider(url)
 
     def start_amazon_spider(self):
         self.DATA = self.AMAZONSPIDER.run_spider()
+        return self.DATA
