3e6f2dd830248f1153db940bb2d83f99141cb876
Homayoon Sadeghi <homayoon.9171@gmail.com>
2021-05-02 16:44:33 +0430

    add Some Column For Our Schema

--- models/Comment.py
+++ models/Comment.py
@@ -12,13 +12,14 @@ class CommentModel(db.Model):
     __tablename__ = 'comments'
 
     id = Column(BigInteger, primary_key=True, autoincrement=True)
-    author = Column(String(100), nullable=True)
+    author = Column(String(100), nullable=False)
     title = Column(Text, nullable=False)
     content = Column(Text, nullable=False)
     is_verified = Column(Boolean, nullable=False, default=True)
-    variant = Column(Text, nullable=False)
-    rating = Column(Float, nullable=False)
+    variant = Column(Text, nullable=True)
+    rating = Column(Float, nullable=True)
     date = Column(Date, nullable=False)
+    sentiment = Column(String(32), nullable=True)
     product_id = Column(BigInteger, db.ForeignKey('products.id'))
     created_at = Column(DateTime(timezone=True), default=db.func.now(), onupdate=db.func.now())
     updated_at = Column(DateTime(timezone=True), default=db.func.now(), onupdate=db.func.now())
