a379bd1300f06032cd36ba3ee1390e277a3f892c
Farid Ahmadpour <aramain.koramain@gmail.com>
2021-05-05 13:25:20 +0430

    Oraganize reviews in a list functionality

--- crawler/SpiderifyWrapper.py
+++ crawler/SpiderifyWrapper.py
@@ -11,4 +11,15 @@ class SpiderifyWrapper:
 
     def start_amazon_spider(self):
         self.DATA = self.AMAZONSPIDER.run_spider()
-        return self.DATA
+
+        return self.organize_reviews(self.DATA)
+
+    def organize_reviews(self, data: dict):
+        organized_data = {}
+        organized_data["REVIEWS"] = []
+        for key in data.keys():
+            if key.startswith("REVIEW #"):
+                organized_data["REVIEWS"].append(data[key])
+            else:
+                organized_data[key] = data[key]
+        return organized_data
