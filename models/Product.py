b88b35599950237a0a1485f1247ce68f8dde6ffb
Farid Ahmadpour <aramain.koramain@gmail.com>
2021-06-02 22:19:25 +0430

    save, delete and __repr__ funcs implemented

--- models/Product.py
+++ models/Product.py
@@ -42,3 +42,14 @@ class ProductModel(db.Model):
         self.rating = rating
         self.description = description
         self.user_id = user_id
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "<Product({id})".format(id=self.id)
