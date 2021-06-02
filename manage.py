fbb9482c6cfea2af4ef9997b77a1421bb210ca2b
Homayoonn <homayoon.9171@gmail.com>
2021-06-02 14:11:02 +0430

    Add Two Functions For Create DB & Destroy DB

--- manage.py
+++ manage.py
@@ -23,6 +23,18 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
+
+@manager.command
+def create_db():
+    """Creates the db tables."""
+    db.create_all()
+
+@manager.command
+def drop_db():
+    """Drops the db tables."""
+    db.drop_all()
+
+
 if __name__ == "__main__":
     manager.run()
 
