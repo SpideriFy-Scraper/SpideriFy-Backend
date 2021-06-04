1a90d33de9a3f2cb8bd62fabbe337416e6c84034
Homayoonn <homayoon.9171@gmail.com>
2021-06-04 11:40:23 +0430

    Complete all APIs For Comment & Product Resources

--- resources/comment.py
+++ resources/comment.py
@@ -1,13 +1,42 @@
-from flask import Response, json, jsonify
-from flask_jwt_extended import jwt_required
-from flask_restful import Resource, reqparse
-
-from common.db import db
+from flask import Response, make_response, jsonify
+from flask_jwt_extended import jwt_required, current_user
+from flask_restful import Resource
 from models.Product import ProductModel
-from models.User import UserModel
 
 
 # ("/product/<string:asin>/comments")--> class CommentsList - -> GET
 class CommentsList(Resource):
-    def get(self):
-        pass
+    @jwt_required()
+    def get(self, asin):
+        """
+        Return all Comments of a given product using asin
+        """
+        product = ProductModel.query.filter_by(
+            asin=asin, user_id=current_user.id).one_or_none()
+        if product:
+            return_product = {
+                "asin": product.asin,
+                "name": product.name,
+                "price": product.price,
+                "rating": product.rating,
+                "description": product.description,
+            }
+            reviews_list = product.comments
+            reviews = []
+            for review in reviews_list:
+                data = {
+                    "author": review.author,
+                    "title": review.title,
+                    "content": review.content,
+                    "verified": review.is_verified,
+                    "variant": review.variant,
+                    "rating": review.rating,
+                    "date": review.date,
+                    "sentiment": review.sentiment,
+                    "summarized content": review.summarized_content,
+                }
+                reviews.append(data)
+            return_product["reviews"] = reviews
+            return make_response(jsonify(return_product), 200)
+        else:
+            return make_response(jsonify({"message": "Failed To Find Such Product"}), 404)
--- resources/product.py
+++ resources/product.py
@@ -26,26 +26,9 @@ class Product(Resource):
                 "rating": product.rating,
                 "description": product.description,
             }
-            reviews_list = product.comments
-            reviews = []
-            for review in reviews_list:
-                data = {
-                    "author": review.author,
-                    "title": review.title,
-                    "content": review.content,
-                    "verified": review.is_verified,
-                    "variant": review.variant,
-                    "rating": review.rating,
-                    "date": review.date,
-                    "sentiment": review.sentiment,
-                    "summarized content": review.summarized_content,
-                }
-                reviews.append(data)
-            return_product["reviews"] = reviews
             return make_response(jsonify(return_product), 200)
         else:
             return make_response(jsonify({"message": "Failed To Find Such Product"}), 404)
-
 
     def post(self):
         pass
@@ -84,8 +67,6 @@ class ProductsList(Resource):
 
 
 # ("/new-product") -> body -> url: str = URL // / JWT = ?
-
-
 class NewProduct(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument(
